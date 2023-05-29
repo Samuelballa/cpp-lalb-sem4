a1050aa9d09d96cbacc421e6511b401a6d82f9c9
Samuelballa <98401731+Samuelballa@users.noreply.github.com>
2023-05-29 10:23:44 +0530

    Delete CPP lab sem4 directory

--- CPP lab sem4/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-import random 
-import pyautogui as pg
-import time
-list =(" maa "," bujji "," kannalu"," kanna amma ","thalli","chitti amma","mummy")
-time.sleep(8)
-for i in range(500):
-    a=random.choice(list)
-    pg.write("i love you  "+a)
-    pg.press("enter")
